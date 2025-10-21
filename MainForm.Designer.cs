8fb1ac4fe81d5eaa0df2ed5c9cf700f4f748fb38
Паламарчук Олександр < @DESKTOP-4GHMTL1>
2025-10-21 21:59:34 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace krCrosshair
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(582, 453);
+            Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "MainForm";
+            Text = "krCrosshair";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
